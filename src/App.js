1e7db7cde6d088e3f2a0e7f01be040e6ea282e7f
Omaima-Shoaib <sim.omaimashoaib3023@alexu.edu.eg>
2024-03-03 19:53:12 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
-
+import {
+  Router,
+  Route,
+  Routes,
+  BrowserRouter,
+  Navigate,
+  redirect,
+} from "react-router-dom";
 function App() {
+  const user = false;
   return (
     <div className="App">
-      <Home></Home>
-      {/* <ProductList></ProductList> */}
-      {/* <Register></Register> */}
-      {/* <Login></Login> */}
-      {/* <Product></Product> */}
-      {/* <Cart></Cart> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/cart" element={<Cart></Cart>}></Route>
+          <Route
+            path="/products/:category"
+            element={<ProductList></ProductList>}
+          ></Route>
+          <Route path="/product/:id" element={<Product></Product>}></Route>
+          <Route path="/login" element={user?<Navigate to='/'></Navigate>:<Login></Login>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
